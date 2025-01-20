317c7014e18571c0e00f9d8effec5daa787b778a
PaulxA09 <pauloarmey18@gmail.com>
2025-01-20 13:59:17 -0500

    Agregar menú de opciones en la función principal

--- to-do-list.py
+++ to-do-list.py
@@ -1,5 +1,27 @@
+# Función de opciones del menu
+def showOptions():
+    print("\nSELECT AN OPTION:\n")
+    print("1. Create a new task")
+    print("2. Mark your tasks")
+    print("3. Delete a task")
+    print("4. Exit")
+
+# Función principal del programa
 def main():
     print(".: WELCOME TO A TO DO LIST IN PYTHON :.")
+    while True:
+        option = int(input("Enter your number option: "))
+        match option:
+            case 1: 
+                print("CREATE A NEW TASK: ")
+            case 2:
+                print("MARK A TASK: ")
+            case 3:
+                print("DELETE A TASK: ")
+            case 4:
+                print("LEAVING THE PROGRAM...")
+            case _:
+                print("Invalid option")
 
 if __name__ == "__main__":
     main()
